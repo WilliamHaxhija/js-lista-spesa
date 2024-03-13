3c0c9ee6afea24e885e3a62b153346cd0acb3a62
WilliamHaxhija <williamhaxhija@gmail.com>
2024-03-13 15:43:20 +0100

    end project

--- js/script.js
+++ js/script.js
@@ -12,3 +12,18 @@ Stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi dell
 Svolgete questo esercizio in 2 versioni: col ciclo for e col ciclo while.
 */
 
+// Esercizio svolto con ciclo for
+const listFor = ['Latte', 'Cacao', 'Insalata', 'Carote', 'Pomodori', 'Piatti pronti'];
+for (let i = 0; i < listFor.length; i++) {
+    const thisItemFor = listFor[i];
+    document.querySelector('#list-for').innerHTML += `<li>${thisItemFor}</li>`;
+}
+
+// Esercizio svolto con ciclo while
+const listWhile = ['Latte', 'Cacao', 'Insalata', 'Carote', 'Pomodori', 'Piatti pronti'];
+let i = 0;
+while (i < listWhile.length) {
+    let thisItemWhile = listWhile[i];
+    document.querySelector('#list-while').innerHTML += `<li>${thisItemWhile}</li>`;
+    i++;
+}
